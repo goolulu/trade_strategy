42eb2e00ac30b773a966e8326bdc5716702cf3fc
hrn <hrnweiruan@outlook.com>
2023-11-09 23:34:39 +0800

    feat: StockTick 新增字段

--- stock_tick_tx.py
+++ stock_tick_tx.py
@@ -22,6 +22,7 @@ class StockTick:
         if trade_date is None:
             stock_zh_a_tick_tx_js_df = ak.stock_zh_a_tick_tx_js(symbol=symbol).rename(columns=self.dict_map)
             stock_zh_a_tick_tx_js_df['symbol'] = symbol[2:]
+            stock_zh_a_tick_tx_js_df['inner_symbol'] = symbol
             stock_zh_a_tick_tx_js_df['trade_date'] = datetime.datetime.now().strftime('%Y%m%d')
             stock_zh_a_tick_tx_js_df['order_type'] = stock_zh_a_tick_tx_js_df['order_type'].apply(convert_order_type)
             reuslt = stock_zh_a_tick_tx_js_df
@@ -31,6 +32,7 @@ class StockTick:
             if stock_zh_a_tick_tx_df.values.size == 0:
                 print("交易日%s 暂时无数据" % trade_date)
                 return
+            stock_zh_a_tick_tx_df['inner_symbol'] = symbol
             stock_zh_a_tick_tx_df['symbol'] = symbol[2:]
             stock_zh_a_tick_tx_df['trade_date'] = datetime.datetime.now().strftime('%Y%m%d')
             stock_zh_a_tick_tx_df['order_type'] = stock_zh_a_tick_tx_df['order_type'].apply(convert_order_type)
