6ccdd0736618352ec6a078716cd960e979e376f5
huangrenning <1715818266@qq.com>
2023-12-21 13:30:40 +0800

    feat: 调整SMA实现

--- indicator/indicator_generator.py
+++ indicator/indicator_generator.py
@@ -9,30 +9,20 @@ from data_source import DataSource
 
 class IndicatorGenerator:
 
-    def SMA(self, data: pd.DataFrame, N: int) -> pd.DataFrame:
-        history = []
-        sma_values = []
-        for close in data['close']:
-            history.append(close)
-            if (len(history) > N):
-                del (history[0])
-            sma_values.append(stats.mean(history))
+    def SMA(self, data: pd.DataFrame, N: int) -> pd.Series:
 
-        figure = plt.figure()
-        ax = figure.subplots()
-        date = data['trade_date'].apply(lambda x: datetime.datetime.strptime(x,'%Y%m%d'))
-        ax.plot(date.values, sma_values, label='sma')
-        ax.plot(date.values, data['close'].values, label='close')
-        ax.set_xlabel("time [day]")
-        ax.set_ylabel("price")
-        ax.legend()
-        ax.plot()
-        figure.show()
+
+        ret = data['close'].rolling(window=N).mean()
+        ret.index = data['trade_date'].apply(lambda x: datetime.datetime.strptime(x, '%Y%M%d'))
+        return ret
 
     def RSI(self):
+
+        data['close'].rolling
+
         pass
 
 
 if __name__ == '__main__':
     data = DataSource().select_list('history', 'hs_stock_quotation')
-    IndicatorGenerator().SMA(data, 5)
+    print(IndicatorGenerator().SMA(data, 5))
