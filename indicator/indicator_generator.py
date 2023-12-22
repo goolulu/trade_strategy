3792b989b64f4f2eabca63e0a0b0460ce92c5398
huangrenning <1715818266@qq.com>
2023-12-22 20:52:03 +0800

    feat: RSI实现

--- indicator/indicator_generator.py
+++ indicator/indicator_generator.py
@@ -3,26 +3,37 @@ import datetime
 import pandas as pd
 import matplotlib.pyplot as plt
 import statistics as stats
-
+import talib
 from data_source import DataSource
 
 
 class IndicatorGenerator:
 
-    def SMA(self, data: pd.DataFrame, N: int) -> pd.Series:
-
-
-        ret = data['close'].rolling(window=N).mean()
-        ret.index = data['trade_date'].apply(lambda x: datetime.datetime.strptime(x, '%Y%M%d'))
+    def SMA(self, data: pd.Series, N: int) -> pd.Series:
+        ret = data.rolling(window=N).mean()
         return ret
 
-    def RSI(self):
+    def TA_SMA(self, data: pd.Series, N: int) -> pd.Series:
+        return talib.SMA(data, timeperiod=N)
 
-        data['close'].rolling
+    def RSI(self, data: pd.Series, N: int):
+        up = data.where(data > 0, 0)
+        down = -data.where(data < 0, 0)
+        up_ma = self.SMA(up, N)
+        down_ma = self.SMA(down, N)
+        rs = up_ma / down_ma
+        rsi = rs * 100
+        return rsi
 
-        pass
+    def ta_RSI(self, data: pd.Series, N: int):
+        return talib.RSI(data, timeperiod=N)
 
 
 if __name__ == '__main__':
-    data = DataSource().select_list('history', 'hs_stock_quotation')
-    print(IndicatorGenerator().SMA(data, 5))
+    data = DataSource().select_list('history', 'hs_stock_quotation').tail(300)
+    sma = IndicatorGenerator().SMA(data['close'], 6)
+    ta_sma = IndicatorGenerator().TA_SMA(data['close'], 6)
+    ta_rsi = IndicatorGenerator().ta_RSI(data['close'], 12)
+    print(ta_rsi)
+    print('fjkla')
+    print(ta_sma)
